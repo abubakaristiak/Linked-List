f198ee2f7d79b92f011c907f84ff57d61cde1914
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-08-17 18:21:17 +0600

    singly linked list related problem

--- singly problem/_02_pr.cpp
+++ singly problem/_02_pr.cpp
@@ -1,9 +1,11 @@
-#include <bits/stdc++.h>
+// // 1 1 2 3 4 2
+
+#include<bits/stdc++.h>
 using namespace std;
 class Node
 {
 public:
-    int val;
+    int val; 
     Node *next;
     Node(int val)
     {
@@ -11,30 +13,70 @@ public:
         this->next = NULL;
     }
 };
-void insert_tail(Node *&head, Node *&tail, int val)
+void insert_at_tail(Node *&head, int val)
 {
     Node *newNode = new Node(val);
-    if (head == NULL)
-    {
+    if(head == NULL){
         head = newNode;
-        tail = newNode;
         return;
     }
-    tail->next = newNode;
-    tail = newNode;
+    Node *tmp = head;
+    while (tmp->next != NULL)
+    {
+        tmp = tmp->next;
+    }
+    tmp->next = newNode;
+    
 }
-
+void find_duplicate(Node * head)
+{
+    Node *cur = head;
+    bool isDuplicate = false;
+    while (cur != NULL)
+    {
+        Node *idx = cur->next;
+        while (idx != NULL)
+        {
+            if(cur->val == idx->val){
+                isDuplicate = true;
+                break;
+            }
+            idx = idx->next;
+        }
+        cur = cur->next; 
+        
+    }
+    if(isDuplicate == true)
+    {
+        cout << "Duplicate"<< endl;
+    }
+    else{
+        cout << "Not Duplicate"<< endl;
+    }
+    
+}
+void printingList(Node *head){
+    Node *tmp = head;
+    while(tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+}
 int main()
 {
     Node *head = NULL;
-    Node *tail = NULL;
     int val;
     while (true)
     {
         cin >> val;
-        if (val == -1)
+        if(val == -1){
             break;
-        insert_tail(head, tail, val);
+        }
+        insert_at_tail(head, val);
+
     }
+    printingList(head);
+    find_duplicate(head);
     return 0;
 }
